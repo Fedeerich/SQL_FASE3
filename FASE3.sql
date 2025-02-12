a1d95bebb599d07d6a32426f6abe8a9f63ffc801
Fedeerich <marcosgongora1000@gmail.com>
2025-02-12 09:56:40 +0100

    12/02/2025

--- FASE3.sql
+++ FASE3.sql
@@ -1,19 +1,19 @@
 /* *****************************************************
   INSTITUT TIC de Barcelona
-    CFGS _____________________________
+    CFGS Desenvolupament d'aplicacions web.
     Mòdul: 0484 Bases de dades. 
-    AUTORS: _______________________________
-    DATA: ________________
+    AUTORS: Ainara Juarez Moreno i Marcos Gongora Carriedo
+    DATA: 12/02/2025
 ****************************************************** */
 
 -- Pregunta 1
-select 'No ho sé';
+select nom, icao from companyia;
 
 -- Pregunta 2
-select 'No ho sé';
+select num_serie, fabricant, any_fabricacio as any, companyia from avio;
 
 -- Pregunta 3
-select 'No ho sé';
+select CONCAT(nom, ciutat, any_construccio) as aeroport from aeroport;
 
 -- Pregunta 4
 select 'No ho sé';
